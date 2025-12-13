3a0fc6200b381f94211ab8338028d4d82074bcbf
navadev-s <navadevsunil3132@gmail.com>
2025-12-13 16:05:47 +0530

    Introduction to objects and class

---
+++ programs/java/Sampleclass.java
@@ -0,0 +1,12 @@
+public class Sampleclass{
+	public static void main(String ar[]){
+		Subsampleclass s1=new Subsampleclass();
+		Subsampleclass s2=new Subsampleclass();
+		s1.a=10;
+		s2.a=50;
+		s1.b=60;
+		s2.b=15;
+		System.out.println("S1-a:"+s1.a+"\nS2-a:"+s2.a+"\nS1-b:"+s1.b+"\nS2-b:"+s2.b);
+	}
+}
+		
---
+++ programs/java/Subsampleclass.java
@@ -0,0 +1,4 @@
+class Subsampleclass{
+	int a;
+	int b;
+}
