a56e014f429896de08f0608319e0f748ecabb7fa
navadev-s <navadevsunil3132@gmail.com>
2025-12-14 19:26:34 +0530

    Prime number using java

---
+++ programs/java/Prime.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Prime{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number :");
+		int n=sc.nextInt();
+		int flag=0;
+		for(int i=2;i<=n/2;i++){
+			if(n%i==0){
+				flag=1;
+				break;
+			}
+		}
+		if(flag==0){
+			System.out.println("The number is prime.");
+		}else{
+			System.out.println("The number is not prime.");
+		}
+	}
+}
